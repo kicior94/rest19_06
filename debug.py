56701e446475cd75a9d6470cfcaa308cc46a6e9a
kicior94 <tomekmarianski@gmail.com>
2021-06-19 11:58:08 +0200

    serializing and deserialzing

--- debug.py
+++ debug.py
@@ -2,6 +2,8 @@ from datetime import datetime
 from games.models import Game
 from games.serializers import GameSerializer
 from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from io import BytesIO
 
 '''
 Baza danych -> Obiekty -> Serializacja -> Render to JSON
@@ -20,3 +22,16 @@ game2_serializer = GameSerializer(game2)
 renderer = JSONRenderer()
 rendered_game1 = renderer.render(game1_serializer.data)
 rendered_game2 = renderer.render(game2_serializer.data)
+
+'''
+Parse from JSON -> Deserializacja -> Obiekt -> Baza danych
+'''
+new_game_json_string = '{"name":"7 Cudow Swiata","release_date":"2021-06-19T08:46:46.170465Z","game_category":"strategic","played":true}'
+new_game_json_bytes = bytes(new_game_json_string, encoding="UTF-8")
+new_game_bytes_stream = BytesIO(new_game_json_bytes)
+parser = JSONParser()
+new_game_parsed = parser.parse(new_game_bytes_stream)
+new_game_serializer = GameSerializer(data=new_game_parsed)
+if new_game_serializer.is_valid():
+    new_game = new_game_serializer.save()
+    print(new_game)
